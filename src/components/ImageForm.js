56cb55cbe045e2dade2b48cc9c6ae715c259c2b1
Max Upravitelev <max@upravitelev.de>
2021-07-16 14:45:28 +0200

    handle image uploading with axios

--- src/components/ImageForm.js
+++ src/components/ImageForm.js
@@ -1,27 +1,22 @@
 import React, { useState } from "react"
 
 // import communication with backend
-// import imagesService from '../services/images'
+import imagesService from '../services/images'
 
 import FormData from 'form-data'
 
 
 const ImageForm = () => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target);
+  const [generatedImageUrl, setGeneratedImageUrl] = useState("")
 
-    const Upload = async () => {
-      await fetch('http://192.168.178.25:6475/api/images', {
-        method: 'POST',
-        body: formData
-        // headers: { 'Content-Type': 'multipart/form-data' }
-      }).then(resp => {
-        resp.json().then(data => { console.log(data) })
-      })
-    }
-    Upload();
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+ 
+        
+    const temp = await imagesService.uploadImagesToBackend(e.target)
+    console.log(temp)
+
   }
 
   return (
@@ -33,6 +28,7 @@ const ImageForm = () => {
           accept="image/*" className="file-custom" />
         <button >GENERATE</button>
       </form>
+      <img src={generatedImageUrl}></img>
     </div>
   )
 }
---
+++ src/services/images.tsx
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import FormData from 'form-data'
+
+// const baseUrl = 'http://localhost:6475/api/'
+const imageUrl = 'http://192.168.178.25:6475/api/images'
+
+
+const uploadImagesToBackend = async (target) => {
+  
+
+  const data = new FormData(target);
+
+  // data.append('styleImage', images.styleImage.file )
+
+//   for(const k of images){
+//     data.append('files',k.file, k.name);
+// }
+
+  const config = {
+    headers: { 
+      // Authorization: token,
+      // "Content-Type": "application/json"
+      // 'Content-Type': 'multipart/form-data'
+      // 'accept': 'application/json',
+      // 'Accept-Language': 'en-US,en;q=0.8',
+      //'Access-Control-Allow-Origin': 'http://192.168.178.25:6475/api/images',
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+      'Content-Type': `multipart/form-data`,
+    },
+    
+  }
+
+  
+
+  const request = axios.post(imageUrl, data, config)
+  console.log(data)
+  console.log(request)
+  const response = await request
+  
+  return response.data
+}
+
+const imagesService = { 
+  uploadImagesToBackend
+}
+
+export default imagesService
