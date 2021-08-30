248ca969833fd145d151fc239ad10a40fd23f0df
Max Upravitelev <max@upravitelev.de>
2021-08-30 13:45:09 +0200

    add StatusLight component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React, { useEffect } from 'react'
 import './App.css'
 import ImageUploader from 'components/ImageUploader/'
+import StatusLight from 'components/StatusLight'
+
 
 // import service for communication with backend
 import imagesService from 'services/images'
 
 const App = () => {
 
-    // sent a ping request for waking up the backend when the frontend is loaded
-    useEffect(() => {
-        imagesService.getPingFromBackend()
-    }, [])
-
     return (
         <div className="App">
+            <StatusLight />
             <ImageUploader />
         </div>
     )
---
+++ src/components/StatusLight.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { Typography } from '@material-ui/core'
+
+import imagesService from 'services/images'
+
+
+const StatusLight = ({ }) => {
+
+  const [backgroundColor, setBackgroundColor] = useState("red") 
+  const [statusDescription, setStatusDescription] = useState("server is offline")
+
+  // sent a ping request for waking up the backend when the frontend is loaded
+  useEffect(() => {
+    const checkState = async () => {
+      const sessionState = await imagesService.getPingFromBackend()
+      
+      console.log(sessionState.data)
+
+      if (sessionState.data == "ping") {
+        setBackgroundColor("green")
+        setStatusDescription("server is online")
+      } else  {
+
+        setBackgroundColor("red")
+        setStatusDescription("server is offline")
+      }
+    }
+    checkState()
+  }, [])
+
+
+    const styleCircle = {
+      backgroundColor: backgroundColor,
+      borderRadius: "50%",
+      height: "2em",
+      width: "2em",
+      display: "inline-block",
+      margin: "auto",
+      marginRight: 5
+
+    }
+
+  return (
+    <div style={{ 
+      // display: 'flex', 
+      marginTop: 30, 
+      marginBottom: 10,
+      alighItems: "center"
+      }}>
+      <div style={styleCircle}></div>
+      <Typography>{statusDescription}</Typography>
+    </div>
+  )
+}
+
+export default StatusLight
