fac76e0ce74e88bb97f2018eeffdbc2ac542aac0
Max Upravitelev <max@upravitelev.de>
2021-08-31 11:01:04 +0200

    add redux, add notification component and reducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import './App.css'
 import ImageUploader from 'components/ImageUploader/'
 import StatusLight from 'components/StatusLight'
+import Notification from 'components/Notification'
 
 
 // import service for communication with backend
@@ -11,8 +12,10 @@ const App = () => {
 
     return (
         <div className="App">
-            <StatusLight />
+            <Notification />
             <ImageUploader />
+            <StatusLight />
+
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+// import redux for state handling
+import { Provider } from 'react-redux'
+import store from './store'
+
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>,
     </React.StrictMode>,
     document.getElementById('root')
 )
