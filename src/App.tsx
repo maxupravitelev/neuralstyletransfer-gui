e12ae4275c2842236671722acb7ced51d9b68b1f
Max Upravitelev <max@upravitelev.de>
2021-09-01 13:18:38 +0200

    add StatusBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import './App.css'
 import ImageUploader from 'components/ImageUploader/'
-import StatusLight from 'components/StatusLight'
 import Notification from 'components/Notification'
 import GeneratedImageDisplay from 'components/GeneratedImageDisplay'
-
+import StatusBar from 'components/StatusBar'
 
 const App = () => {
 
     return (
         <div className="App">
             <Notification />
+            <StatusBar />
             <ImageUploader />
-            <StatusLight />
             <GeneratedImageDisplay />
         </div>
     )
---
+++ src/components/StatusBar/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import StatusLight from './StatusLight'
+
+// import material ui components & styles
+import { makeStyles } from '@material-ui/styles'
+import { styles } from 'styles'
+
+const useStyles = makeStyles(styles)
+
+
+const StatusBar = () => {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.imagePreviewContainer}>
+            <StatusLight />
+        </div>
+    )
+}
+
+export default StatusBar
